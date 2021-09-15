9cefbc26c2faf623e6e686691dc216f4b59c330f
Rianna <rianna.cleary@gmail.com>
2021-09-15 11:23:24 -0400

    updated dream seaquence

--- Desktop/sonic_pi/dream_seaquence.rb
+++ Desktop/sonic_pi/dream_seaquence.rb
@@ -1,19 +1,22 @@
 # Dream Seaquence
 
-live_loop :beginners_dream do
-    use_synth :dark_ambience
-    play choose([:C3, :E3]), release: 6, attack: 6, amp: 2
-    sleep 4
+use_bpm 112
+drum_array = [1,0,2,0,1,1,0,1,0,1,2,2,1,0,2,0]
+
+live_loop :beat do
+  16.times do |i|
+    sample :bd_zome if drum_array[i] === 1
+    sample :bd_sone if drum_array[i] === 2
+    sleep 0.25
   end
-  
-  live_loop :curious_faries do
-    use_synth :dark_ambience
-    play choose([:G4, :B4]), release: 5, attack: 4, amp: 2
-    sleep 4
-  end
-  
-  live_loop :bright_star do
-    use_synth :dark_ambience
-    play choose([:D4, :A5]), release: 5, attack: 5, amp: 2
-    sleep 4
-  end
+end
+
+16.times do
+  use_synth :dark_ambience
+  play choose([:C3, :E3]), release: 6, amp: 3
+  sleep 4
+  play choose([:G4, :B4]), release: 5, amp: 3
+  sleep 4
+  play choose([:D4, :A5]), release: 5, amp: 3
+  sleep 4
+end

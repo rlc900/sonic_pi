495ada5820de0e26a810ce8958ad49780550159b
Rianna <rianna.cleary@gmail.com>
2021-09-16 10:57:12 -0400

    updated tron

--- Desktop/sonic_pi/tron_legasea.rb
+++ Desktop/sonic_pi/tron_legasea.rb
@@ -1,11 +1,21 @@
 # TRON Legasea
 live_loop :tron do
-    s = synth :dsaw, note: :F3, release: 8, sustain: 4
-    sleep 3
-    control s, note: :E3
-    sleep 0.5
-    control s, note: :D3
-    sleep 0.25
-    control s, note: :A3
-    sleep 2
-  end
+  s = synth :dsaw, note: :F3, sustain: 7.1
+  sleep 3
+  control s, note: :E3
+  sleep 0.5
+  control s, note: :D3
+  sleep 0.25
+  control s, note: :A3
+  sleep 4.25
+end
+
+live_loop :drive do
+  sample :bd_haus, amp: 3
+  sleep 0.5
+end
+
+live_loop :industry do
+  sample :loop_industrial, beat_stretch: 1
+  sleep 1
+end
